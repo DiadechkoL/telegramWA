75cceb94be6619939d703488fd178c8f143438e0
unknown <liza.dyadechko@gmail.com>
2024-06-06 15:58:20 +0300

    fixed finction lizaBot

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,24 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const {Telegraf} = require("telegraf");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const bot = new Telegraf(functions.config().telegram.token, {
+  telegram: {webhookReply: true},
+});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+bot.catch((err, ctx) => {
+  functions.logger.error("[Bot] Error", err);
+  return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err);
+});
+
+
+bot.command("/start", (ctx) => ctx.reply("Hello!"));
+bot.on("message", (ctx) => ctx.reply("Check out my app at: https://t.me/LizaD_MyApp_Bot/LizaD "));
+
+
+exports.lizaBot = functions.https.onRequest(async (request, response) => {
+  functions.logger.log("Incoming message", request.body);
+  await bot.handleUpdate(request.body, response);
+
+  response.sendStatus(200);
+});
